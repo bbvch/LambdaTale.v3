0ffc88970ce755766966ab2322d0373b760cc53f
Dominik Baumann <dominik.baumann@bbv.ch>
2025-08-27 16:40:52 +0200

    Add remining things to CombinedTestFramework

--- src/LambdaTale.v3/Framework/CombinedTestFramework.cs
+++ src/LambdaTale.v3/Framework/CombinedTestFramework.cs
@@ -5,11 +5,18 @@ namespace LambdaTale.v3.Framework;
 
 public class CombinedTestFramework : TestFramework
 {
+    private readonly string? configFileName;
+
+    public CombinedTestFramework() { }
+
+    public CombinedTestFramework(string? configFilename) =>
+        this.configFileName = configFilename;
+
     protected override ITestFrameworkDiscoverer CreateDiscoverer(Assembly assembly) =>
-        new LambdaTaleDiscoveryFacade(new(assembly));
+        new LambdaTaleDiscoveryFacade(new(assembly, this.configFileName));
 
     protected override ITestFrameworkExecutor CreateExecutor(Assembly assembly) =>
-        new LambdaTaleExecutorFacade(new(assembly));
+        new LambdaTaleExecutorFacade(new(assembly, this.configFileName));
 
     public override string TestFrameworkDisplayName => "LambdaTale Combined Test Framework";
 }
--- src/LambdaTale.v3/Framework/LambdaTaleExecutorFacade.cs
+++ src/LambdaTale.v3/Framework/LambdaTaleExecutorFacade.cs
@@ -1,4 +1,5 @@
-﻿using Xunit.Sdk;
+﻿using LambdaTale.v3.Execution;
+using Xunit.Sdk;
 using Xunit.v3;
 
 namespace LambdaTale.v3.Framework;
@@ -6,17 +7,15 @@ namespace LambdaTale.v3.Framework;
 public class LambdaTaleExecutorFacade(TestAssemblyFacade testAssembly)
     : TestFrameworkExecutor<ITestCase>(testAssembly)
 {
-    private new TestAssemblyFacade TestAssembly { get; } = testAssembly;
+    public ScenarioTestAssembly LambdaTaleAssembly { get; } = new(
+        testAssembly.Assembly, testAssembly.ConfigFilePath);
 
-    private readonly XunitTestFrameworkExecutor xunitExecutor = new(
-        new XunitTestAssembly(testAssembly.Assembly, testAssembly.ConfigFilePath,
-            testAssembly.Assembly.GetName().Version, testAssembly.UniqueID));
-
-    private readonly LambdaTaleExecutor lambdaTaleExecutor = new(
-        new ScenarioTestAssembly(testAssembly.Assembly, testAssembly.ConfigFilePath));
+    public IXunitTestAssembly XunitTestAssembly { get; } = new XunitTestAssembly(
+        testAssembly.Assembly, testAssembly.ConfigFilePath, testAssembly.Assembly.GetName().Version,
+        testAssembly.UniqueID);
 
     protected override ITestFrameworkDiscoverer CreateDiscoverer() =>
-        new LambdaTaleDiscoveryFacade(this.TestAssembly);
+        new LambdaTaleDiscoveryFacade(testAssembly);
 
     public override async ValueTask RunTestCases(
         IReadOnlyCollection<ITestCase> testCases,
@@ -38,9 +37,32 @@ public class LambdaTaleExecutorFacade(TestAssemblyFacade testAssembly)
             }
         }
 
-        await this.xunitExecutor.RunTestCases(
-            xunitTestCases, executionMessageSink, executionOptions, cancellationToken);
-        await this.lambdaTaleExecutor.RunTestCases(
-            lambdaTaleTestCases, executionMessageSink, executionOptions, cancellationToken);
+        // TODO: Right now the summary will only include the first of this invocations. This is because of the runners
+        //  Shutting down after handling the first "AssemblyFinished" message
+        await RunLambdaTaleTests(this.LambdaTaleAssembly, lambdaTaleTestCases.Cast<ScenarioTestCase>().ToArray(),
+            executionMessageSink, executionOptions, cancellationToken);
+
+        await RunXunitTests(this.XunitTestAssembly, xunitTestCases,
+            executionMessageSink, executionOptions, cancellationToken);
     }
+
+    private static async ValueTask RunXunitTests(
+        IXunitTestAssembly testAssembly,
+        IReadOnlyCollection<IXunitTestCase> testCases,
+        IMessageSink executionMessageSink,
+        ITestFrameworkExecutionOptions executionOptions,
+        CancellationToken cancellationToken) =>
+        await XunitTestAssemblyRunner.Instance.Run(testAssembly, testCases, executionMessageSink, executionOptions,
+            cancellationToken);
+
+
+    private static async ValueTask RunLambdaTaleTests(
+        ScenarioTestAssembly testAssembly,
+        ScenarioTestCase[] testCases,
+        IMessageSink executionMessageSink,
+        ITestFrameworkExecutionOptions executionOptions,
+        CancellationToken cancellationToken) =>
+        await ScenarioTestAssemblyRunner.Instance.Run(testAssembly, testCases, executionMessageSink,
+            executionOptions,
+            cancellationToken);
 }
