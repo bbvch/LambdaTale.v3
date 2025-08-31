719092fc32a904059b96a41af244e6956cf0686c
Dominik Baumann <dominik.baumann@bbv.ch>
2025-08-31 10:25:19 +0200

    Change attribute to opt out of xUnit1013 analyzer

--- src/LambdaTale.v3/ScenarioAttribute.cs
+++ src/LambdaTale.v3/ScenarioAttribute.cs
@@ -3,6 +3,7 @@
 namespace LambdaTale.v3;
 
 [AttributeUsage(AttributeTargets.Method)]
+[IgnoreXunitAnalyzersRule1013]
 public class ScenarioAttribute(
     [CallerFilePath] string? sourceFilePath = null,
     [CallerLineNumber] int sourceLineNumber = -1) : Attribute
@@ -10,4 +11,12 @@ public class ScenarioAttribute(
     public bool DisableDiscoveryEnumeration { get; }
 
     public bool SkipTestWithoutData { get; }
+
+    public string? SourceFilePath { get; } = sourceFilePath;
+
+    public int? SourceLineNumber { get; } = sourceLineNumber < 1 ? null : sourceLineNumber;
+}
+
+public sealed class IgnoreXunitAnalyzersRule1013Attribute : Attribute
+{
 }
