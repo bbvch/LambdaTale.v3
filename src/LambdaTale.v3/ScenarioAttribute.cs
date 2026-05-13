ae8c239bf22a0ff56139b692c358e4dc5db994f0
Dominik Baumann <dominik.baumann@bbv.ch>
2026-05-13 20:19:22 +0000

    Directly inherit [FactAttribute] instead of interface
    This gives us gutter "play" icons and the likes for "free"

--- src/LambdaTale.v3/ScenarioAttribute.cs
+++ src/LambdaTale.v3/ScenarioAttribute.cs
@@ -1,40 +1,17 @@
 using System.Runtime.CompilerServices;
+using Xunit;
 using Xunit.v3;
 
 namespace LambdaTale.v3;
 
 [AttributeUsage(AttributeTargets.Method)]
 [XunitTestCaseDiscoverer(typeof(ScenarioDiscoverer))]
-[IgnoreXunitAnalyzersRule1013]
 public sealed class ScenarioAttribute(
-    [CallerFilePath] string? sourceFilePath = null,
-    [CallerLineNumber] int sourceLineNumber = -1) : Attribute, IFactAttribute
+    [CallerFilePath] string sourceFilePath = "",
+    [CallerLineNumber] int sourceLineNumber = -1)
+    : FactAttribute(sourceFilePath, sourceLineNumber)
 {
     public bool SkipTestWithoutData { get; init; }
-
-    public string? DisplayName { get; init; }
-
-    public bool Explicit { get; init; }
-
-    public Type[]? SkipExceptions { get; init; }
-
-    public Type? SkipType { get; init; }
-
-    public string? SkipUnless { get; init; }
-
-    public string? SkipWhen { get; init; }
-
-    public int Timeout { get; init; }
-
-    // IFactAttribute
-    public string? Skip { get; init; }
-    public string? SourceFilePath { get; } = sourceFilePath;
-    public int? SourceLineNumber { get; } = sourceLineNumber < 1 ? null : sourceLineNumber;
-}
-
-[AttributeUsage(AttributeTargets.Class)]
-public sealed class IgnoreXunitAnalyzersRule1013Attribute : Attribute
-{
 }
 
 [AttributeUsage(AttributeTargets.Method)]
